c626f4daa85b2910b759d549c276ab3b3894fb0e
UNILORN <b5159@oic.jp>
2016-09-09 23:35:04 +0900

    testdata

--- practice2_function.php
+++ practice2_function.php
@@ -7,47 +7,47 @@
   }
 
   function write_sql($data){
-      $dns = "mysql:host=127.0.0.1;dbname=php;charset=utf8";
-      $pdo = new PDO($dns,"testuser","");
-
-      $sql = "insert into php_test(name,created_at,updated_at) values('".mb_convert_encoding($data,'UTF-8')."',now(),now());";
-      $stmh = $pdo -> prepare($sql);
-      $stmh->execute();
+      // $dns = "mysql:host=127.0.0.1;dbname=php;charset=utf8";
+      // $pdo = new PDO($dns,"testuser","");
+      //
+      // $sql = "insert into php_test(name,created_at,updated_at) values('".mb_convert_encoding($data,'UTF-8')."',now(),now());";
+      // $stmh = $pdo -> prepare($sql);
+      // $stmh->execute();
   }
 
   function delete_sql($data){
-      $dns = "mysql:host=127.0.0.1;dbname=php;charset=utf8";
-      $pdo = new PDO($dns,"testuser","");
-
-      $sql = "delete from php_test where id in(";
-      foreach ($data as $key => $value){
-        if ($key == 0){$sql = $sql."$value";}
-        else{$sql = $sql.",$value";}
-      }
-      $sql = $sql.");";
-      $stmh = $pdo -> prepare($sql);
-      $stmh->execute();
+      // $dns = "mysql:host=127.0.0.1;dbname=php;charset=utf8";
+      // $pdo = new PDO($dns,"testuser","");
+      //
+      // $sql = "delete from php_test where id in(";
+      // foreach ($data as $key => $value){
+      //   if ($key == 0){$sql = $sql."$value";}
+      //   else{$sql = $sql.",$value";}
+      // }
+      // $sql = $sql.");";
+      // $stmh = $pdo -> prepare($sql);
+      // $stmh->execute();
 
   }
   function edit_sql($data,$id){
-
-    $dns = "mysql:host=127.0.0.1;dbname=php;charset=utf8";
-    $pdo = new PDO($dns,"testuser","");
-
-    $sql = "update php_test set name = '".$data."' where id = ".$id." ;";
-    $stmh = $pdo -> prepare($sql);
-
-    $stmh->execute();
+    //
+    // $dns = "mysql:host=127.0.0.1;dbname=php;charset=utf8";
+    // $pdo = new PDO($dns,"testuser","");
+    //
+    // $sql = "update php_test set name = '".$data."' where id = ".$id." ;";
+    // $stmh = $pdo -> prepare($sql);
+    //
+    // $stmh->execute();
   }
 
   function alldelete_sql(){
 
-    $dns = "mysql:host=127.0.0.1;dbname=php;charset=utf8";
-    $pdo = new PDO($dns,"testuser","");
-
-    $sql = "truncate table php_test;";
-    $stmh = $pdo -> prepare($sql);
-
-    $stmh->execute();
+    // $dns = "mysql:host=127.0.0.1;dbname=php;charset=utf8";
+    // $pdo = new PDO($dns,"testuser","");
+    //
+    // $sql = "truncate table php_test;";
+    // $stmh = $pdo -> prepare($sql);
+    //
+    // $stmh->execute();
   }
  ?>
